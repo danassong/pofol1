7b97a6eabf176f848b733e4cb4b884394ef6fc71
DanaSong <dahae0624@naver.com>
2020-09-28 18:01:24 +0900

    0928

--- js/common.js
+++ js/common.js
@@ -4,17 +4,58 @@
     var colorCode = ['#28BDBE', '#7CC6C6', '#B0DADA', '#DAEDED']
     var minCnt = setInterval(minusCount, 500)
     var k = 5
+
     function minusCount() {
         k--;
-        if (k===0) {
+        if (k === 0) {
             clearInterval(minCnt)
             $('.introAni').fadeOut(500)
             return false
         }
         $('.introAni i').css({
-            color: colorCode[k-1]
-        }) 
+            color: colorCode[k - 1]
+        })
     }
+
+
+    init()
+
+    var flag = true
+
+    function init() {
+        var ww = $(window).width()
+        if (ww > 767 && flag) {
+            $('.logoNav .nav').show()
+            $('.open_nav, .close_nav').hide()
+            flag = false
+        } else if (ww <= 767 && !flag) {
+            $('.open_nav').show()
+            $('.logoNav .nav').hide()
+            flag = true
+        }
+    }
+
+
+    $(window).on('resize', function () {
+        init()
+    })
+
+    // nav 햄버거 버튼
+    $('.logoNav .open_nav').on('click', function () {
+        $(this).next().stop().slideDown(300)
+        $(this).hide()
+        $(this).nextAll('.close_nav').css({
+            display: 'block'
+        })
+    })
+    $('.logoNav .close_nav').on('click', function () {
+        $(this).prev().stop().slideUp(300)
+        $(this).hide()
+        $(this).prevAll('.open_nav').css({
+            display: 'block'
+        })
+    })
+
 
 
     /* PAGE 로드 분리 */
@@ -42,7 +83,7 @@
     var sct = 0;
     $(window).scroll(function () {
         sct = $(this).scrollTop();
-        
+
         //헤더 상단
         if (sct >= 200) {
             // 내리면
@@ -58,7 +99,7 @@
                 paddingTop: '25px',
                 paddingBottom: '25px',
             })
-            $('.open-gnb').css({
+            $('.open_nav').css({
                 paddingTop: '22.5px',
                 paddingBottom: '22.5px',
             })
@@ -79,7 +120,7 @@
                 paddingTop: '30px',
                 paddingBottom: '30px',
             })
-            $('.open-gnb').css({
+            $('.open_nav').css({
                 paddingTop: '27.5px',
                 paddingBottom: '27.5px',
             })
@@ -92,7 +133,7 @@
         /// 스크롤이벤트가 발생하면 business.html에서
         /// .businessSec 구역을 찾아서 함수 호출해 애니메이션 수행하게
 
-        if ( $('#secContainer').children().is('.businessSec') ) {
+        if ($('#secContainer').children().is('.businessSec')) {
             bizScroll()
         }
     });
@@ -108,10 +149,10 @@
 
 
 
-        var img3Near = $('#img3arr').offset().top - $(this).height()/2
+        var img3Near = $('#img3arr').offset().top - $(this).height() / 2
 
         if (sct >= img3Near) {
-            for ( var i = 0; i < $('#img3arr div').length; i++) {
+            for (var i = 0; i < $('#img3arr div').length; i++) {
                 $('#img3arr div').eq(i).css({
                     animationDelay: i * 0.3 + 's'
                 }).addClass('fadeInUp')
@@ -122,10 +163,10 @@
 
 
 
-        var sc2Near = $('#seccol2').offset().top - $(this).height()/2
+        var sc2Near = $('#seccol2').offset().top - $(this).height() / 2
 
         if (sct >= sc2Near) {
-            for ( var i = 0; i < $('#seccol2 div').length; i++) {
+            for (var i = 0; i < $('#seccol2 div').length; i++) {
                 $('#seccol2 div').eq(i).css({
                     animationDelay: i * 0.3 + 's'
                 }).addClass('fadeInUp')
@@ -145,10 +186,10 @@
 
 
 
-        var img4Near = $('#img4arr').offset().top - $(this).height()/1.5
+        var img4Near = $('#img4arr').offset().top - $(this).height() / 1.5
 
         if (sct >= img4Near) {
-            for ( var i = 0; i < $('#img4arr a').length; i++) {
+            for (var i = 0; i < $('#img4arr a').length; i++) {
                 $('#img4arr a').eq(i).css({
                     animationDelay: i * 0.2 + 's'
                 }).addClass('fadeInUp')
